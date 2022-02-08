2176418b83f6e9eae0d75212736daaed5c9d8e54
Noah Kalish <noah@spacemule.net>
2022-02-09 01:23:24 +0200

    added per-user rsync

--- push.sh
+++ push.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 SERVER_ROOT=${HOME}/Development/configs/pod/server-files/
-rsync -rlptDvz --exclude "**/nginx/ssl" $SERVER_ROOT root@pod.local:/
+rsync -rlptDvz --exclude "**/home" $SERVER_ROOT root@pod.local:/
+
+while IFS= read -r SERVER_USER
+do
+	rsync -rlptDvz --exclude "**/nginx/ssl" ${SERVER_ROOT}home/${SERVER_USER}/ ${SERVER_USER}@pod.local:/home/${SERVER_USER}
+done < ${HOME}/Development/configs/pod/users
 
 ssh root@pod.local "echo \"Files owned by root in /home:\" &&  find /home/ -user root -maxdepth 4"
 
