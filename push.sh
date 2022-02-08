ebf62d0edd17db27b24495879cbfb423b81169ba
Noah Kalish <noah@spacemule.net>
2022-02-09 01:01:39 +0200

    prevent overwrite ssl keys

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SERVER_ROOT=${HOME}/Development/configs/pod/server-files/
-rsync -rlptDvz $SERVER_ROOT root@pod.local:/
+rsync -rlptDvz --exclude "**/nginx/ssl" $SERVER_ROOT root@pod.local:/
 
 ssh root@pod.local "echo \"Files owned by root in /home:\" &&  find /home/ -user root -maxdepth 4"
 
